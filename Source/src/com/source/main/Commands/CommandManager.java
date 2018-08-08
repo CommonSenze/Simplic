9650a3853177bfff446c0215a8fcee0385a45ae1
CommonSenze <commonsenze00@gmail.com>
2018-08-07 23:13:06 -0500

    Update

--- Source/src/com/source/main/Commands/CommandManager.java
+++ Source/src/com/source/main/Commands/CommandManager.java
@@ -9,30 +9,46 @@ import org.bukkit.command.CommandSender;
 
 import com.source.main.Main;
 import com.source.main.Commands.Util.Commands;
+import com.source.main.Utils.Function;
 import com.source.main.Utils.Settings;
 
-public class CommandManager implements CommandExecutor {
+public class CommandManager extends Function implements CommandExecutor {
 
+	private Main plugin;
 	private ArrayList<Commands> commands = new ArrayList<>();
-	
-	public CommandManager(Main plugin) {}
-	
+
+	public CommandManager(Main plugin) {
+		this.plugin = plugin;
+		plugin.add(this);
+	}
+
 	public void add(Commands command) {
 		if (Bukkit.getPluginCommand(command.getCommand()) != null) {
 			commands.add(command);
+			plugin.getLogger().info(Settings.PLUGIN_PREFIX + "/"+ command.getCommand() + " has been added to list of commands.");
 		}
 	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String string, String[] args) {
 		for (Commands command : commands) {
-			try {
-				command.run();
-			} catch (Exception e) {
-				sender.sendMessage(Settings.UNKNOWN_ERROR);
-				e.printStackTrace();
+			if (command.getCommand().equalsIgnoreCase(cmd.getName())) {
+				try {
+
+					command.run();
+				} catch (Exception e) {
+					sender.sendMessage(Settings.UNKNOWN_ERROR);
+					e.printStackTrace();
+				}
 			}
 		}
 		return false;
+	}
+	
+	
+
+	@Override
+	public void unload() {
+		commands.clear();
 	}
 }
--- Source/src/com/source/main/Utils/Settings.java
+++ Source/src/com/source/main/Utils/Settings.java
@@ -4,6 +4,7 @@ import net.md_5.bungee.api.ChatColor;
 
 public class Settings {
 
+	public static final String PLUGIN_PREFIX = "SimplicSource >> ";
 	public static final String PERMISSIONS_FILE = "Permissions";
 	public static final String UNKNOWN_ERROR = ChatColor.RED + "An unknown error has occured.";
 }
